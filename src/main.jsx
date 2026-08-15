8562f5f4cf5841576fa047a4ec9beeca20f90118
shreephad985-spec <shreephad985@gmail.com>
2026-08-15 20:35:30 +0530

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React, { StrictMode } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
